8ce6ccf5b37ab1cba02dc1a7f8247b79ae279d2b
njine <brikab@gmail.com>
2024-04-13 02:25:07 +0300

    racl

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,18 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
+export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
-    .all([uploadPhoto(), createUser()])
+    .all([uploadPhoto(fileName), createUser(firstName, lastName)])
     .then((res) => {
-      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+      const photoResponse = res[0];
+      const userResponse = res[1];
+
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+      return { photo: photoResponse, user: userResponse };
     })
-    .catch(() => console.log('Signup system offline'));
+    .catch(() => {
+      console.log('Signup system offline');
+      return { photo: null, user: null };
+    });
 }
 
