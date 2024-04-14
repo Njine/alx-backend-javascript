3ec5ebfa78b71697fe3b7ff8f2f7aad03938006e
njine <brikab@gmail.com>
2024-04-15 01:13:52 +0300

    charto

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,15 +1,15 @@
 export default class Building {
   constructor(sqft) {
-    this._sqft = sqft;
-    if (this.constructor === Building) {
-      throw new Error('Abstract class Building cannot be instantiated directly');
-    }
-    if (typeof this.evacuationWarningMessage !== 'function') {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
+      this._sqft = sqft;
+      if (this.constructor === Building) {
+          throw new Error('Abstract class Building cannot be instantiated directly');
+      }
+      if (typeof this.evacuationWarningMessage !== 'function') {
+          throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
   }
 
   get sqft() {
-    return this._sqft;
+      return this._sqft;
   }
 }
