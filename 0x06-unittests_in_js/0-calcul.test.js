588cd5325246b0c819b1d3cb6b67132b47b80d98
njine <brikab@gmail.com>
2024-06-07 03:25:09 +0300

    moch

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,39 +1,20 @@
-const { expect } = require('chai');
+const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
-  it('should return 3 for (1.0, 2.0)', () => {
-    expect.hasAssertions();
-    expect(calculateNumber(1.0, 2.0)).toStrictEqual(3);
+  it('should return 4 for (1, 3)', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
   });
 
-  it('should return 3 for (1.0, 2.4)', () => {
-    expect.hasAssertions();
-    expect(calculateNumber(1.0, 2.4)).toStrictEqual(3);
+  it('should return 5 for (1, 3.7)', () => {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
 
-  it('should return 3 for (1.4, 2.4)', () => {
-    expect.hasAssertions();
-    expect(calculateNumber(1.4, 2.4)).toStrictEqual(3);
+  it('should return 5 for (1.2, 3.7)', () => {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
 
-  it('should return 4 for (1.0, 2.5)', () => {
-    expect.hasAssertions();
-    expect(calculateNumber(1.0, 2.5)).toStrictEqual(4);
+  it('should return 6 for (1.5, 3.7)', () => {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
-
-  it('should return 6 for (2.6, 2.5)', () => {
-    expect.hasAssertions();
-    expect(calculateNumber(2.6, 2.5)).toStrictEqual(6);
-  });
-
-  it('should return 5 for (2.6, 2.0)', () => {
-    expect.hasAssertions();
-    expect(calculateNumber(2.6, 2.0)).toStrictEqual(5);
-  });
-
-  it('should return 5 for (2.499999, 3.499999)', () => {
-    expect.hasAssertions();
-    expect(calculateNumber(2.499999, 3.499999)).toStrictEqual(5);
-});
 });
