c0b9d09e518c388bfc4774495241dc8c16b96eed
njine <brikab@gmail.com>
2024-06-07 05:57:02 +0300

    mocha 10

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 7865;
+
+app.use(bodyParser.json());
 
 app.get('/', (_, res) => {
   res.send('Welcome to the payment system');
--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -4,30 +4,32 @@ const { expect } = require('chai');
 describe('API integration test', () => {
   const API_URL = 'http://localhost:7865';
 
-  it('GET /available_payments returns correct response', (done) => {
-    request.get(`${API_URL}/available_payments`, (_err, res, body) => {
-      const expectedResponse = {
-        payment_methods: {
-          credit_cards: true,
-          paypal: false
-        }
-      };
+  it('GET / returns correct response', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
-      expect(JSON.parse(body)).to.deep.equal(expectedResponse);
+      expect(body).to.be.equal('Welcome to the payment system');
       done();
     });
   });
 
-  it('POST /login returns correct response with username', (done) => {
-    const username = 'Betty';
+  it('GET /available_payments returns valid response', (done) => {
+    request.get(`${API_URL}/available_payments`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(JSON.parse(body))
+        .to.be.deep.equal({ payment_methods: { credit_cards: true, paypal: false } });
+      done();
+    });
+  });
+
+  it('POST /login returns valid response', (done) => {
     request.post(
       {
         url: `${API_URL}/login`,
-        json: { userName: username }
+        json: { userName: 'Pinkbrook' }
       },
       (_err, res, body) => {
         expect(res.statusCode).to.be.equal(200);
-        expect(body).to.be.equal(`Welcome ${username}`);
+        expect(body).to.be.equal('Welcome Pinkbrook');
         done();
       }
     );
